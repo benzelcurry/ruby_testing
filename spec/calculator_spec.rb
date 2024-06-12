65548ae5fa1f4108bdb2c01fe3d5684081ecaaca
Ben <benawhiting@gmail.com>
2024-06-12 15:33:18 -0700

    Write a test for a division method

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -12,4 +12,11 @@ describe Calculator do
       expect(calculator.add(2, 5, 7)).to eql(14)
     end
   end
+
+  describe '#divide' do
+    it 'returns the quotient of two numbers' do
+      calculator = Calculator.new
+      expect(calculator.divide(16, 8)).to eql(2)
+    end
+  end
 end
